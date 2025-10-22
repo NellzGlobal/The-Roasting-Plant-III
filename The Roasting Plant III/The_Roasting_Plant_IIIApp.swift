bc4bcb3905e2206ee5f4f3e1c209607d7faa405e
Nell Mister <mister.s.do@gmail.com>
2025-10-22 10:11:17 -0400

    Initial Commit

---
+++ The Roasting Plant III/The_Roasting_Plant_IIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Roasting_Plant_IIIApp.swift
+//  The Roasting Plant III
+//
+//  Created by Dornell Mister on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct The_Roasting_Plant_IIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
