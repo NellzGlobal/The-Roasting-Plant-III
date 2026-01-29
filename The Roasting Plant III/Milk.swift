c0f7d379c4db47627794b6b830e051377f671870
DuckNellz <shachiorange@gmail.com>
2026-01-29 08:50:01 -0500

    Saving Assets

---
+++ The Roasting Plant III/Milk.swift
@@ -0,0 +1,14 @@
+//
+//  Size.swift
+//  The Roasting Plant III
+//
+//  Created by Dornell Mister on 10/23/25.
+//
+
+import Foundation
+
+enum Size: String, CaseIterable {
+    case Small
+    case Medium
+    case Large
+}
